8d4c7b1f4b78d936194bbf8b859f4bd42bfc480f
Benoit Daloze <eregontp@gmail.com>
2015-12-16 17:42:46 +0100

    [Truffle] Optimize deep sharing.
    * An AST now mimics the message structure to very efficiently share the whole message.
    * Optimize Queue and SizedQueue.
    * Share Arrays and their Object[] storage.

--- truffle/src/main/java/org/jruby/truffle/core/queue/SizedQueueNodes.java
+++ truffle/src/main/java/org/jruby/truffle/core/queue/SizedQueueNodes.java
@@ -28,7 +28,7 @@ import org.jruby.truffle.core.cast.BooleanCastWithDefaultNodeGen;
 import org.jruby.truffle.core.thread.ThreadManager.BlockingAction;
 import org.jruby.truffle.language.RubyNode;
 import org.jruby.truffle.language.control.RaiseException;
-import org.jruby.truffle.language.objects.shared.SharedObjects;
+import org.jruby.truffle.language.objects.shared.PropagateSharingNode;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.locks.ReentrantLock;
@@ -131,6 +131,8 @@ public abstract class SizedQueueNodes {
     })
     public abstract static class PushNode extends CoreMethodNode {
 
+        @Child PropagateSharingNode propagateSharingNode = PropagateSharingNode.create();
+
         public PushNode(RubyContext context, SourceSection sourceSection) {
             super(context, sourceSection);
         }
@@ -144,7 +146,7 @@ public abstract class SizedQueueNodes {
         public DynamicObject pushBlocking(DynamicObject self, final Object value, boolean nonBlocking) {
             final BlockingQueue<Object> queue = Layouts.SIZED_QUEUE.getQueue(self);
 
-            SharedObjects.propagate(self, value);
+            propagateSharingNode.propagate(self, value);
             doPushBlocking(value, queue);
 
             return self;
@@ -165,7 +167,7 @@ public abstract class SizedQueueNodes {
         public DynamicObject pushNonBlock(DynamicObject self, final Object value, boolean nonBlocking) {
             final BlockingQueue<Object> queue = Layouts.SIZED_QUEUE.getQueue(self);
 
-            SharedObjects.propagate(self, value);
+            propagateSharingNode.propagate(self, value);
             final boolean pushed = doOffer(value, queue);
             if (!pushed) {
                 CompilerDirectives.transferToInterpreter();
